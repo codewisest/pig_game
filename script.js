06b5f3719b61068e91c33629d5338d2f47851d88
Chijioke <wisestcodes@gmail.com>
2023-05-25 10:57:06 -0400

    update current score

--- script.js
+++ script.js
@@ -1,29 +1,66 @@
 'use strict';
 // get DOM elements
 const rollDice = document.querySelector('.btn--roll');
+const hold = document.querySelector('.btn--hold');
 const players = document.querySelectorAll('.player');
 const playersScore = document.querySelectorAll('[class*="score"');
 const activePlayer = document.querySelector('.player--active');
+const dice = document.querySelector('.dice');
 
 // initialise game
 function initialiseGame() {
   for (let i = 0; i < playersScore.length; i++) {
     playersScore[i].innerHTML = 0;
+    dice.style.display = 'none';
   }
 }
 
+initialiseGame();
+
+let randomNumber;
 // generate random number 1 to 6
-function randomNumber() {
-  return Math.trunc(Math.random() * 6) + 1;
+const randomResult = function () {
+  randomNumber = Math.trunc(Math.random() * 6) + 1;
+  console.log(randomNumber);
+  return randomNumber;
+};
+
+// set dice image
+function setDiceImage() {
+  dice.setAttribute('src', `dice-${randomNumber}.png`);
+  dice.style.display = 'block';
 }
 
-initialiseGame();
+// switch active player
+function toggleActivePlayer() {
+  for (let i = 0; i < players.length; i++) {
+    players[i].classList.toggle('player--active');
+  }
+}
+
+// update current player current score
+let scoreUpdate = 0;
+
+function updateCurrentScore() {
+  const activePlayerCurrentScoreDOM =
+    activePlayer.querySelector('.current-score');
+  //   let currentScoreDOM = activePlayerCurrentScoreDOM.textContent;
+  //   let currentScore = Number(currentScoreDOM);
+  scoreUpdate += randomNumber;
+  activePlayerCurrentScoreDOM.textContent = scoreUpdate;
+  console.log(scoreUpdate);
+}
 
 // event handlers
 // on dice roll
 rollDice.addEventListener('click', function () {
   // generate random number 1 to 6
-  console.log(randomNumber());
+  randomResult();
   // display corresponding dice image
+  setDiceImage();
+
   // add to current player score
+  updateCurrentScore();
 });
+
+// on hold
