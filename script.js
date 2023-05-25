c8ff3f9fc5a450952aa89df5fabe6035b544d222
Chijioke <wisestcodes@gmail.com>
2023-05-25 10:15:09 -0400

    generate random number

--- script.js
+++ script.js
@@ -12,12 +12,18 @@ function initialiseGame() {
   }
 }
 
+// generate random number 1 to 6
+function randomNumber() {
+  return Math.trunc(Math.random() * 6) + 1;
+}
+
 initialiseGame();
 
 // event handlers
 // on dice roll
 rollDice.addEventListener('click', function () {
   // generate random number 1 to 6
+  console.log(randomNumber());
   // display corresponding dice image
   // add to current player score
 });
