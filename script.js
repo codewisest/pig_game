2566c1a07db0d8e0e83bab88ff53f2f8ab8adfa8
Chijioke <wisestcodes@gmail.com>
2023-05-25 10:08:04 -0400

    initialise game

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+// get DOM elements
+const rollDice = document.querySelector('.btn--roll');
+const players = document.querySelectorAll('.player');
+const playersScore = document.querySelectorAll('[class*="score"');
+const activePlayer = document.querySelector('.player--active');
+
+// initialise game
+function initialiseGame() {
+  for (let i = 0; i < playersScore.length; i++) {
+    playersScore[i].innerHTML = 0;
+  }
+}
+
+initialiseGame();
+
+// event handlers
+// on dice roll
+rollDice.addEventListener('click', function () {
+  // generate random number 1 to 6
+  // display corresponding dice image
+  // add to current player score
+});
